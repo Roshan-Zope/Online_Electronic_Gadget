ea368253d22ec58cac67a3ce885c1d368085e0c3
Roshan-Zope <roshanzope456@gmail.com>
2024-07-04 09:07:07 +0530

    bug fixes

--- app/src/main/java/com/example/onlineelectronicgadget/fragments/AdminHomeScreen.java
+++ app/src/main/java/com/example/onlineelectronicgadget/fragments/AdminHomeScreen.java
@@ -7,13 +7,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.onlineelectronicgadget.R;
+import com.example.onlineelectronicgadget.adapters.CategoryAdapter;
+import com.example.onlineelectronicgadget.adapters.ProductHomeAdapter;
+import com.example.onlineelectronicgadget.database.DatabaseHelper;
+import com.example.onlineelectronicgadget.models.Product;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AdminHomeScreen extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -21,6 +35,14 @@ public class AdminHomeScreen extends Fragment {
     private String mParam1;
     private String mParam2;
     private FloatingActionButton addProductButton;
+    private DatabaseHelper db;
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+    private RecyclerView categoryRecyclerView;
+    private CategoryAdapter categoryAdapter;
+    private ProductHomeAdapter adapter;
+    private List<Product> list;
+    private List<String> categories;
 
     public AdminHomeScreen() {
         // Required empty public constructor
@@ -48,14 +70,65 @@ public class AdminHomeScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_admin_home_screen, container, false);
-        addProductButton = view.findViewById(R.id.addProductButton);
+        initComponent(view);
+        loadProductByCategory("tablet");
         return view;
+    }
+
+    private void loadProductByCategory(String category) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("category", category);
+
+        Log.d("myTag", "in loadProductByCategory()");
+        progressBar.setVisibility(View.VISIBLE);
+
+        db.search(map, products -> {
+            Log.d("myTag", "in loadProductByCategory()" + products.toString());
+            list.clear();
+            list.addAll(products);
+            adapter.notifyDataSetChanged();
+        });
+
+        progressBar.setVisibility(View.GONE);
+    }
+
+    private void initComponent(View view) {
+        addProductButton = view.findViewById(R.id.addProductButton);
+        db = new DatabaseHelper();
+        progressBar = view.findViewById(R.id.progressBar);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        categoryRecyclerView = view.findViewById(R.id.categoryRecyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        categoryRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        categories = new ArrayList<>();
+        list = new ArrayList<>();
+    }
+
+    private void loadCategories() {
+        categories.add("Tablet");
+        categories.add("Laptop");
+        categories.add("Tv");
+        categories.add("Watch");
+        categoryAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         addProductButton.setOnClickListener(v -> onAddProductButton());
+
+        categoryAdapter = new CategoryAdapter(categories, category -> {
+            loadProductByCategory(category.toLowerCase());
+            Log.d("myTag", category.toLowerCase());
+        });
+        loadCategories();
+        categoryRecyclerView.setAdapter(categoryAdapter);
+
+        adapter = new ProductHomeAdapter(list, product -> {
+            loadFragment(new ProductViewFragment(product));
+        });
+
+        recyclerView.setAdapter(adapter);
     }
 
     private void onAddProductButton() {
